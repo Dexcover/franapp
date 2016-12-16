729d674ad81805cef12c076131cabbbdd2e2159c
Diego V Chicaiza H <diegochicaiza1@hotmail.com>
2016-12-16 10:23:36 -0600

    actualizacion

--- application/controllers/ccrearcliente.php
+++ application/controllers/ccrearcliente.php
@@ -18,5 +18,31 @@ class Ccrearcliente extends CI_Controller
 		$this->load->view('vcrearcliente');
 		$this->load->view('layout/footer');
 	}
+
+
+
+public function crearcliente()
+		{
+
+			//call to varibales in the view
+			$id_cliente=$_POST['cliente'];
+			$id_usuario=$this->session->userdata('s_idUsuario');
+			$n_cliente=$_POST['nombre'];
+			$ruc=$_POST['ruc'];
+
+			//do query
+			$this->Mcliente->crearCliente($id_cliente,$id_usuario,$n_cliente,$ruc);
+
+		}
+
+		public function verclientes()
+		{
+
+			//call data from model
+			$data['clientes']=$this->Mcliente->obtenerClientes();
+			$this->load->view('', $data);
+
+		}
+	
 }
 ?>
