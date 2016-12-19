96e55c238d912ba2622140051fc0c7d8833ba4f2
Diego V Chicaiza H <diegochicaiza1@hotmail.com>
2016-12-19 10:40:31 -0600

    no message

--- application/models/mstudio.php
+++ application/models/mstudio.php
@@ -26,17 +26,17 @@ public function crearEstudio($id_usuario,$id_source,$id_cliente,$id_tipo,$num_co
 		if($id_usuario==1)
 		{
 			$sql = "SELECT TIPO.N_TIPO, estudio.FISCALYEAR FROM ESTUDIO, TIPO, SOURCE, USUARIOS, ACTORES WHERE\n"
-	    . "estudio.ID_USUARIO=usuarios.ID_USUARIO AND\n"
-	    . "estudio.ID_SOURCE=source.ID_SOURCE AND\n"
-	    . "estudio.ID_CLIENTE=actores.ID_CLIENTE AND\n"
-	    . "estudio.ID_TIPO=tipo.ID_TIPO";
+	    . "estudio.ID_USUARIO=USUARIOS.ID_USUARIO AND\n"
+	    . "estudio.ID_SOURCE=SOURCE.ID_SOURCE AND\n"
+	    . "estudio.ID_CLIENTE=ACTORES.ID_CLIENTE AND\n"
+	    . "estudio.ID_TIPO=TIPO.ID_TIPO";
 		
 		$query=$this->db->query($sql);
 		return $query->result_array();
 		}else{
 
 		$this->db->where('ID_USUARIO', $id_usuario);
-		$query=$this->db->get('estudio');
+		$query=$this->db->get('ESTUDIO');
 		return $query->result_array();
 
 		}
